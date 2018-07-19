26ab27b66ab8b53ac05c3e7866ca001f475d908f
Philippe Moneda <philippemoneda@gmail.com>
2018-07-19 17:52:06 -0300

    adiciona o freeze na retificacao e cria a app de simulacao

--- NodeJS/bankapp/retificacao-cotacao/process/retificacao-cotacao.js
+++ NodeJS/bankapp/retificacao-cotacao/process/retificacao-cotacao.js
@@ -6,6 +6,8 @@ SDK.run((context, resolve) => {
     Object.keys(params).forEach(attr => {
         cotacao[attr] = params[attr];
     })
+    //freeze bloqueia a alteração da data de modificacao do objeto
+    cotacao._metadata.freeze = true
     context.dataset.Cotacao.update(cotacao);
     resolve();
 });
